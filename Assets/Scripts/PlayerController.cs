efbc24d01c4821c7134128cc91cda4f5cb5de90e
julienBr <julien.bringard@outlook.fr>
2023-02-21 15:24:12 +0100

    Update Player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speedH;
     [SerializeField] private float _speedV;
     [SerializeField] private float _jump;
-    private Animator _camAnimator;
     private Animator _animator;
     private Rigidbody _body;
     private bool _lookBack;
+    private Vector3 _currentPosition;
 
     private void Awake()
     {
-        _camAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
         _animator = GetComponent<Animator>();
         _body = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
+        _currentPosition = transform.position;
         Move();
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
@@ -26,34 +26,27 @@ public class PlayerController : MonoBehaviour
             else LookForward();
         }
         if (Input.GetKeyDown(KeyCode.Space)) Jump();
-        if (Input.GetKeyDown(KeyCode.LeftShift)) Slide();
     }
 
     private void Move()
     {
-        _body.velocity = transform.right * _speedV;
-        //transform.Translate(_speedV * Time.deltaTime,0f, Input.GetAxis("Horizontal") * -_speedH * Time.deltaTime);
+        transform.Translate(Input.GetAxis("Horizontal") * _speedH * Time.deltaTime, 0f, _speedV * Time.deltaTime);
     }
 
     private void LookBack()
     {
         _lookBack = true;
-        _camAnimator.SetBool("LookBack", true);
+        _animator.SetBool("LookBack", true);
     }
 
     private void LookForward()
     {
         _lookBack = false;
-        _camAnimator.SetBool("LookBack", false);
+        _animator.SetBool("LookBack", false);
     }
 
     private void Jump()
     {
         _body.AddForce(0f, _jump, 0f);
-    }
-
-    private void Slide()
-    {
-        _animator.SetTrigger("Slide");
     }
 }
