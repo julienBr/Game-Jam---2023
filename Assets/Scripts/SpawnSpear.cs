c3f124579a813e55c805c40f4e01e851001ab0e1
Farnights <120110552+Farnights@users.noreply.github.com>
2023-02-22 15:13:45 +0100

    Update

--- Assets/Scripts/SpawnSpear.cs
+++ Assets/Scripts/SpawnSpear.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,19 @@ using UnityEngine;
 public class SpawnSpear : MonoBehaviour
 {
     [SerializeField] private AppDatas _appDatas;
+
+    private void OnEnable()
+    {
+        CameraController.SpearFall += SpearSpawn;
+    }
+    
+    private void OnDisable()
+    {
+        CameraController.SpearFall -= SpearSpawn;
+    }
+
+    private void SpearSpawn()
+    {
+        
+    }
 }
