1018d47412cf1c19dd634a7648b60dbed1ee8303
julienBr <julien.bringard@outlook.fr>
2023-02-22 13:42:28 +0100

    Update LooseWindow

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -4,7 +4,24 @@ using TMPro;
 public class GameUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text timer;
+    [SerializeField] private GameObject _looseWindow;
 
+    private void OnEnable()
+    {
+        CameraController.PlayerDeath += DisplayLooseWindow;
+    }
+
+    private void OnDisable()
+    {
+        CameraController.PlayerDeath -= DisplayLooseWindow;
+    }
+
+    private void DisplayLooseWindow()
+    {
+        Time.timeScale = 0f;
+        _looseWindow.SetActive(true);
+    }
+    
     private void Update()
     {
         timer.text = $"{Mathf.Floor(Timer.time / 60):0}:{Timer.time % 60:00}";
