5ca478f583fe7360d3970adcece1df368d181bb3
julienBr <julien.bringard@outlook.fr>
2023-02-22 09:41:31 +0100

    Update Menu

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _fade;
+	
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+	private void NewGame()
+	{
+		StartCoroutine(ThrowFade());
+	}
+
+	private IEnumerator ThrowFade()
+	{
+		_fade.GetComponent<Animator>().SetTrigger("FadeOut");
+		yield return new WaitForSeconds(1f);
+		SceneManager.LoadScene("1_Level_1");
+	}
+}
